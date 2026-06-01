56031cecd2976c6b78840adeaacc8b9dad218dfe
Tayyab2802 <tayyabkhalid024@gmail.com>
2026-06-01 16:25:57 +0100

    vpc module added and configured to dev

---
+++ environments/dev/outputs.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+  description = "ID of the dev VPC"
+  value       = module.networking.vpc_id
+}
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -2,3 +2,27 @@ variable "aws_region" {
   description = "AWS region"
   type        = string
 }
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
