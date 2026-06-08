ae52c010ea273a7af1eb16b3da5c0f9ac19829a6
Tayyab2802 <tayyabkhalid024@gmail.com>
2026-06-08 15:29:56 +0100

    Allow GitHub OIDC access from pull request workflows

--- modules/github_oidc/main.tf
+++ modules/github_oidc/main.tf
@@ -26,10 +26,17 @@ resource "aws_iam_role" "github_actions" {
           Federated = aws_iam_openid_connect_provider.github.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
+
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/${var.github_branch}"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/${var.github_branch}",
+              "repo:${var.github_org}/${var.github_repo}:pull_request"
+            ]
           }
         }
       }
